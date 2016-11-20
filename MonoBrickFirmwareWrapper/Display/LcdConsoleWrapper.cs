631972bd098863108a0b21a3fe5446966e5eb541
Geroshabu <oonstks@gmail.com>
2016-11-20 23:29:52 +0900

    refs #3 ✨ Add wrapper methods

--- MonoBrickFirmwareWrapper/Display/LcdConsoleWrapper.cs
+++ MonoBrickFirmwareWrapper/Display/LcdConsoleWrapper.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonoBrickFirmware.Display;
 
 namespace MonoBrickFirmwareWrapper.Display
 {
 	public static class LcdConsoleWrapper
 	{
+		public delegate void WriteLineDelegate(string format, params object[] arg);
+
+		public static WriteLineDelegate WriteLine { get; } = LcdConsole.WriteLine;
+
+		public static Action Clear { get; } = LcdConsole.Clear;
 	}
 }
