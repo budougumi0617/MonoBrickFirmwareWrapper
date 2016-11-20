49a648acf1e3cf2491001dd08d2955a95c7c45c6
Geroshabu <oonstks@gmail.com>
2016-11-20 23:32:28 +0900

    refs #3 📝 Add documentation comments

--- MonoBrickFirmwareWrapper/Display/LcdConsoleWrapper.cs
+++ MonoBrickFirmwareWrapper/Display/LcdConsoleWrapper.cs
@@ -7,12 +7,26 @@ using MonoBrickFirmware.Display;
 
 namespace MonoBrickFirmwareWrapper.Display
 {
+	/// <summary>
+	/// A wrapper class of <see cref="LcdConsole"/>.
+	/// </summary>
 	public static class LcdConsoleWrapper
 	{
+		/// <summary>
+		/// Delegate type of <see cref="LcdConsole.WriteLine"/>.
+		/// </summary>
+		/// <param name="format">A composite format string.</param>
+		/// <param name="arg">An array of objects to write using format.</param>
 		public delegate void WriteLineDelegate(string format, params object[] arg);
 
+		/// <summary>
+		/// A wrapper of <see cref="LcdConsole.WriteLine"/>.
+		/// </summary>
 		public static WriteLineDelegate WriteLine { get; } = LcdConsole.WriteLine;
 
+		/// <summary>
+		/// A wrapper of <see cref="LcdConsole.Clear"/>.
+		/// </summary>
 		public static Action Clear { get; } = LcdConsole.Clear;
 	}
 }
