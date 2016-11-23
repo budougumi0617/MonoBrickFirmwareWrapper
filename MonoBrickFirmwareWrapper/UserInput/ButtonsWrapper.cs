63fd841aa936f55be3a18d1cea983c0fb00fbb82
Geroshabu <oonstks@gmail.com>
2016-11-23 22:34:31 +0900

    refs #4 ✨ Add wrapper methods

--- MonoBrickFirmwareWrapper/UserInput/ButtonsWrapper.cs
+++ MonoBrickFirmwareWrapper/UserInput/ButtonsWrapper.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using MonoBrickFirmware.UserInput;
 
 namespace MonoBrickFirmwareWrapper.UserInput
 {
 	public static class ButtonsWrapper
 	{
+		public static Func<Buttons.ButtonStates> GetStates { get; } = Buttons.GetStates;
+
+		public static Action<CancellationToken> WaitForKeyReleaseCancelable { get; } = Buttons.WaitForKeyRelease;
+
+		public static Action WaitForKeyRelease { get; } = Buttons.WaitForKeyRelease;
+
+		public static Func<CancellationToken, Buttons.ButtonStates> GetKeypressCancelable { get; } = Buttons.GetKeypress;
+
+		public static Func<Buttons.ButtonStates> GetKeypress { get; } = Buttons.GetKeypress;
+
+		public static Action<int> LedPattern { get; } = Buttons.LedPattern;
 	}
 }
