86eba8e66fbe1e60f9fd068ca7919ebdd5d4bd70
Geroshabu <oonstks@gmail.com>
2016-11-23 22:34:58 +0900

    refs #4 📝 Add documentation comments

--- MonoBrickFirmwareWrapper/UserInput/ButtonsWrapper.cs
+++ MonoBrickFirmwareWrapper/UserInput/ButtonsWrapper.cs
@@ -8,18 +8,39 @@ using MonoBrickFirmware.UserInput;
 
 namespace MonoBrickFirmwareWrapper.UserInput
 {
+	/// <summary>
+	/// A wrapper class of <see cref="Buttons"/>.
+	/// </summary>
 	public static class ButtonsWrapper
 	{
+		/// <summary>
+		/// A wrapper of <see cref="Buttons.GetStates"/>.
+		/// </summary>
 		public static Func<Buttons.ButtonStates> GetStates { get; } = Buttons.GetStates;
 
+		/// <summary>
+		/// A wrapper of <see cref="Buttons.WaitForKeyRelease(CancellationToken)"/>.
+		/// </summary>
 		public static Action<CancellationToken> WaitForKeyReleaseCancelable { get; } = Buttons.WaitForKeyRelease;
 
+		/// <summary>
+		/// A wrapper of <see cref="Buttons.WaitForKeyRelease"/>.
+		/// </summary>
 		public static Action WaitForKeyRelease { get; } = Buttons.WaitForKeyRelease;
 
+		/// <summary>
+		/// A wrapper of <see cref="Buttons.GetKeypress(CancellationToken)"/>.
+		/// </summary>
 		public static Func<CancellationToken, Buttons.ButtonStates> GetKeypressCancelable { get; } = Buttons.GetKeypress;
 
+		/// <summary>
+		/// A wrapper of <see cref="Buttons.GetKeypress"/>.
+		/// </summary>
 		public static Func<Buttons.ButtonStates> GetKeypress { get; } = Buttons.GetKeypress;
 
+		/// <summary>
+		/// A wrapper of <see cref="Buttons.LedPattern(int)"/>
+		/// </summary>
 		public static Action<int> LedPattern { get; } = Buttons.LedPattern;
 	}
 }
