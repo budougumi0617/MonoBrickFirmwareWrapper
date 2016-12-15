c8a44915ae53c37c326bbe9b9f4bd66d59444fc2
Geroshabu <oonstks@gmail.com>
2016-12-16 00:10:42 +0900

    refs #24 ✨ Add methods to replace wrapper methods more simply

--- MonoBrickFirmwareWrapper/Utilities/Replacer.cs
+++ MonoBrickFirmwareWrapper/Utilities/Replacer.cs
@@ -772,5 +772,175 @@ namespace MonoBrickFirmwareWrapper.Utilities
         {
             ReplacePrivateStaticField(targetClassType, methodName, function);
         }
+
+		public static void SetWrapperMethod<TInstance>(this TInstance instance, string methodName, Action action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T>(this TInstance instance, string methodName, Action<T> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2>(this TInstance instance, string methodName, Action<T1, T2> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3>(this TInstance instance, string methodName, Action<T1, T2, T3> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4>(this TInstance instance, string methodName, Action<T1, T2, T3, T4> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(this TInstance instance, string methodName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> action)
+		{
+			instance.SetPrivateField(methodName, action);
+		}
+
+		public static void SetWrapperMethod<TInstance, TResult>(this TInstance instance, string methodName, Func<TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T, TResult>(this TInstance instance, string methodName, Func<T, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, TResult>(this TInstance instance, string methodName, Func<T1, T2, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
+
+		public static void SetWrapperMethod<TInstance, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this TInstance instance, string methodName, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> function)
+		{
+			instance.SetPrivateField(methodName, function);
+		}
     }
 }
